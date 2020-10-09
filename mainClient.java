3517105ed7cc3a5256d0ec889b7b6348c85d7a44
tdchambers <50087924+tdchambers@users.noreply.github.com>
2020-10-09 00:31:02 -0400

    Update mainClient.java

--- mainClient.java
+++ mainClient.java
@@ -2,8 +2,13 @@
 	
 public class mainClient{
 	public static void main(String[] args){
-		new GUI();
-			
+	
+		GUI Gui= new GUI();
+		Gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		 Gui.layoutView();
+		 Gui.setSize(300,300);
+		 Gui.setVisible(true);
 	}
 	}
 	

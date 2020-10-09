d0b6972f34be01af8ae8b781dd0d7425fa6810e3
siwe9840 <72326150+siwe9840@users.noreply.github.com>
2020-10-08 22:13:20 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,11 +2,8 @@
 import java.io.*;
 import java.net.*;
 
-
-public class SocketClient
-{
+public class SocketClient {
 	private Socket clientSocket; 
-	
 	private DataInputStream  din   = null; 
 	private DataOutputStream dout     = null; 
 	
@@ -17,8 +14,7 @@ public class SocketClient
 	}
 	
 		
-	public void connect (String location, int port) throws IOException
-	{
+	public void connect(String location, int port){
 	 	try
 		{
 		System.out.println("Conneting to server " +location+ " on port " + port);
@@ -30,8 +26,7 @@ public class SocketClient
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	} catch(IOException e){
 		e.printStackTrace();
-		throw new IOException("Connection refused. Enter a new address and Port.");
-		}	
+	}
 	}
 	
 		
@@ -44,47 +39,20 @@ public class SocketClient
 		else{
 			requestData = requestData + "ISBN: " + ISBN + "\r\n" + "TITLE: " + TITLE + "\r\n" + "AUTHOR: " + AUTHOR + "\r\n" + "PUBLISHER: " + PUBLISHER + "\r\n" + "YEAR: " + YEAR + "\r\n";
 			}
-		    		
-
-		/*
-		String[] split = serverResponse.split("\r\n");
-		   for (String data : split) {
-			String[] splitLine = x.split(" ");
-                    if (splitLine[0].contains("ISBN")) {
-                        serverResponse = serverResponse.concat("@BookSubmission{\r\n\tISBN\t= \"" + x.substring(splitLine[0].length()).trim() + "\",\r\n");
-                    }
-                   else if (splitLine[0].contains("TITLE")) {
-                        serverResponse = serverResponse.concat("\tTITLE\t= \"" + x.substring(splitLine[0].length()).trim() + "\",\r\n");
-                    }
-                   else if (splitLine[0].contains("AUTHOR")) {
-                        serverResponse = serverResponse.concat("\tAUTHOR\t= \"" + x.substring(splitLine[0].length()).trim() + "\",\r\n");
-                    }
-                   else if (splitLine[0].contains("PUBLISHER")) {
-                        serverResponse = serverResponse.concat("\tPUBLISHER\t= \"" + x.substring(splitLine[0].length()).trim() + "\",\r\n");
-                    }
-                    else if (splitLine[0].contains("YEAR")) {
-                            serverResponse = serverResponse.concat("\tYEAR\t= \"" + x.substring(splitLine[0].length()).trim() + "\",\r\n");
-                    }
-                }
-		*/
 		return requestData;	
-        }
-	catch(IOException e)
-	{
-		e.printStackTrace(); 		
-        }	
+  	
+	}
         
 
-public String sendMessage( Request request, String ISBN, String TITLE, String AUTHOR, String PUBLISHER, int YEAR,
+public String sendMessage(Request request, String ISBN, String TITLE, String AUTHOR, String PUBLISHER, int YEAR,
                               boolean all){
-	String processedRequest = processRequest(request, ISBN, TITLE, AUTHOR, PUBLISHER, YEAR, all);
-	dout.println(processRequest + "\r\n\\EOF");
+	String processedMessage = processMessage(request, ISBN, TITLE, AUTHOR, PUBLISHER, YEAR, all);
+	dout.println(processedMessage + "\r\n\\EOF");
 	String response = getServerResponse();
 
 	return response;
-	
-
 }
+	
 public String getServerResponse(){
 	String serverResponse = "";
 	String line = din.readLine();

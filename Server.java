e9715b4450273454e4a19545b5ac1875ac4e5507
siwe9840 <72326150+siwe9840@users.noreply.github.com>
2020-10-08 20:53:19 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -275,39 +275,10 @@ public class SocketServer extends Thread{
                                      
                   }
               }
+}
 
             
-                  
- public class mainServer{
-       
-      public static void main(String[] args) throws IOException{
-            
-            int port;
-            if(args[0].length() ==0) {
-            port =5000;
-            }
-            else{
-                  port = Integer.parseInt(args[0]);
-            }
-            ServerSocket serverSocket = new ServerSocket(port);
-            start(serverSocket);
-            }
-   
-       
-      public static void start(ServerSocket serverSocket){
-           ArrayList<BookSubmission> bookSubmissions = new ArrayList<BookSubmission>();
-
-            try{
-                  System.out.println("Waiting to be connected");
-                  Socket clientSocket = serverSocket.accept();
-                  SocketServer socketServer = new SocketServer(clientSocket, bookSubmissions);
-                  socketServer.start();
-            
-      }catch(IOException e){
-                  e.printStackTrace();
- }
-      }
- }
+                 
 
             
             
